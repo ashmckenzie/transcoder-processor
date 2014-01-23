1a3b6926113bced8cae47038abe94807e77c75a9
Ash McKenzie <ash@greenworm.com.au>
2014-01-23 23:55:30 +1100

    Connect to a defined redis URL

--- config/initialisers/sidekiq.rb
+++ config/initialisers/sidekiq.rb
@@ -1,10 +1,12 @@
 require 'sidekiq'
 require 'sidekiq-failures'
 
+redis_options = { url: TranscoderProcessor::Config.instance.redis.url, namespace: 'transcoder-processor' }
+
 Sidekiq.configure_client do |config|
-  config.redis = { namespace: 'transcoder-processor' }
+  config.redis = redis_options
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { namespace: 'transcoder-processor' }
+  config.redis = redis_options
 end
