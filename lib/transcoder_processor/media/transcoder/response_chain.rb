571a64b7890ac671cd5d625fdd597fc93d42e3f3
Ash McKenzie <ash@greenworm.com.au>
2014-02-13 08:09:56 +1100

    Fixes jobs not completing

--- lib/transcoder_processor/media/transcoder/response_chain.rb
+++ lib/transcoder_processor/media/transcoder/response_chain.rb
@@ -12,15 +12,29 @@ module TranscoderProcessor
         end
 
         def success?
-          @success ||= responses.detect { |x| !x.success? } ? false : true
+          @success ||= (responses.detect { |x| !x.success? }) ? false : true
         end
 
         def status
           success? ? Status::SUCCESSFUL : Status::FAILED
         end
 
+        def exit_code
+          exit_code = -1
+
+          responses.each do |x|
+            exit_code = if (x.exit_code.exitstatus > exit_code)
+              x.exit_code.exitstatus
+            else
+              exit_code
+            end
+          end
+
+          exit_code
+        end
+
         def output
-          ''
+          responses.map { |x| x.output }.join("\n\n").chomp
         end
 
         private
